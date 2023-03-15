59acf6431210ab9f48539b0fec14c273dc92f3d0
remaz <remaz1ne@live.com>
2023-03-15 07:19:00 +0300

    adding api viewsets

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls'))
+    path('restaurant/', include('restaurant.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path("menu/", views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
+    # path("booking/", views.bookingview.as_view()),
+    path('booking/', include(router.urls)),
+
 ]
